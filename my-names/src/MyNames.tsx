1d3e53629fb2e92ca41078c058b8e65359025f55
takeru.hashimoto <hstake25@gmail.com>
2022-12-30 00:45:33 +0900

    fix export

--- my-names/src/MyNames.tsx
+++ my-names/src/MyNames.tsx
@@ -2,10 +2,8 @@ type Props = {
   name: string;
 }
 
-const MyNames: React.FC<Props> = (props) => {
+export const MyNames: React.FC<Props> = (props) => {
   const { name } = props;
 
   return <div>{name}</div>;
 }
-
-export default MyNames;
