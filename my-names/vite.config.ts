caaec66367d11e84d1d349e7e0dacc0d79f5ac16
takeru.hashimoto <hstake25@gmail.com>
2022-12-30 13:52:04 +0900

    lint

--- my-names/vite.config.ts
+++ my-names/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import { createBaseConfig } from "../vite.base.config";
-import { resolve } from "path";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { createBaseConfig } from '../vite.base.config';
 
 export default defineConfig({
   ...createBaseConfig({
-    entry: resolve(__dirname, 'src/index.ts')
-  })
-})
+    entry: resolve(__dirname, 'src/index.ts'),
+  }),
+});
